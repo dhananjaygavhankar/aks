b8244eaf2dc08922a9d1b0c33be61d2282632157
Dhananjay Gavhankar <dhananjaygavhankar@outlook.com>
2025-09-23 20:31:07 +0530

    azure env setup

--- Parent/Provider.tf
+++ Parent/Provider.tf
@@ -1,20 +1,31 @@
+#============== KINDLY RUN BELLOW COMMAND========== 
+# $env:ARM_SUBSCRIPTION_ID = (az account show --query id -o tsv)
+
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.39.0"
+      version = "4.45.0"
     }
   }
   backend "azurerm" {
     resource_group_name  = "DoNotDeleteRg"
-    storage_account_name = "donotdeletestorage555"
+    storage_account_name = "donotdeletestorage5555"
     container_name       = "tfstate"
-    key                  = "akstfstate"
-    subscription_id      = "3a45f28c-3b05-4d1b-b640-21585be45e39"
+    key                  = "aks_terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "3a45f28c-3b05-4d1b-b640-21585be45e39"
+  # use_cli = true
+  # subscription_id = "13dfd1e6-6770-4299-b7ea-6aa09b346468"
+}
+
+
+data "azurerm_client_config" "current" {}
+
+output "Output_subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
 }
